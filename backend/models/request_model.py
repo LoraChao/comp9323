233b017d5dd742baef1fccba5c259950f2e82ae7
zyfozone <85334354+zyfozone@users.noreply.github.com>
2022-07-16 14:43:54 +0800

    Update request_model.py

--- backend/models/request_model.py
+++ backend/models/request_model.py
@@ -89,3 +89,31 @@ follow_model = api.model("followed", {
     "Company": fields.List(fields.Integer),
     "Individual": fields.List(fields.Integer)
 })
+
+offer_model = api.model("offer post", {
+    "OrganizationId": fields.Integer,
+    "Salary": fields.String,
+    "Workinghours": fields.String,
+    "Tag": fields.String
+})
+
+search_organizations_model = api.model("search organization", {
+    "OfferId": fields.Integer,
+    "Salary": fields.String,
+    "Workinghours": fields.String,
+    "Tag": fields.String
+})
+
+post_dairy_model = api.model("dairy post", {
+    "IndividualId": fields.Integer,
+    "IndividualName": fields.String,
+    "Date": fields.String,
+    "Content": fields.String
+})
+
+search_dairy_model = api.model("dairy search", {
+    "IndividualId": fields.Integer,
+    "IndividualName": fields.String,
+    "Date": fields.String,
+    "Content": fields.String
+})
