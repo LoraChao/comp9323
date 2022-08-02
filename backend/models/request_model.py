e3d122089ec2ee7fe9455b50e57327b1e7c2ad5f
zyfozone <85334354+zyfozone@users.noreply.github.com>
2022-08-02 16:24:38 +0800

    Add files via upload

--- backend/models/request_model.py
+++ backend/models/request_model.py
@@ -2,55 +2,101 @@ from flask_restx import fields
 from flask_app import api
 
 
-# predefine model
+#Ziyue
 individual_model = api.model("individual", {
-    "IndividualName": fields.String,
-    "Password": fields.String,
-    "Preference": fields.String,
-    "Occupation": fields.String,
+    "username": fields.String,
+    "password": fields.String,
 })
 
 organization_model = api.model("organization", {
-    "OrganizationName": fields.String,
+    "username": fields.String,
     # "email": fields.String,
     "password": fields.String,
     # "preference": fields.String,
 })
 
 login_model = api.model("login", {
-    "Name": fields.String,
+    "username": fields.String,
     "password": fields.String
 })
 
 offer_model = api.model("offer post", {
     "OrganizationId": fields.Integer,
-    "Salary": fields.String,
-    "Workinghours": fields.String,
-    "Tag": fields.String
+    "company_name": fields.String,
+    "position_name": fields.String,
+    "working_location_name": fields.String,
+    "working_hour_name": fields.String,
+    "salary_name": fields.String,
+    "responsibility_name": fields.String,
+    "requirement_name": fields.String,
+    "contact_nam": fields.String
 })
 
 search_organizations_model = api.model("search organization", {
-    "OfferId": fields.Integer,
-    "Salary": fields.String,
-    "Workinghours": fields.String,
-    "Tag": fields.String
+    "OrganizationId": fields.Integer
+})
+
+search_organizations_details_model = api.model("search organization details", {
+    "OrganizationId": fields.Integer,
+    "OfferId": fields.Integer
 })
 
 post_dairy_model = api.model("dairy post", {
     "IndividualId": fields.Integer,
-    "IndividualName": fields.String,
-    "Date": fields.String,
-    "Content": fields.String
+    "RecordTime": fields.String,
+    "Mood": fields.String,
 })
 
 search_dairy_model = api.model("dairy search", {
     "IndividualId": fields.Integer,
-    "IndividualName": fields.String,
-    "Date": fields.String,
-    "Content": fields.String
+    "RecordTime": fields.String,
 })
 
-#shell's work
+brief_individual_model = api.model("brief individual get", {
+    "userId": fields.Integer,
+})
+
+post_individual_model = api.model("post individual model", {
+    "username": fields.String,
+    "title_name": fields.String,
+    "name_name": fields.String,
+    "gender_name": fields.String,
+    "age_name": fields.String,
+    "email_name": fields.String,
+    "skill_name": fields.String,
+    "education_name": fields.String,
+    "experience_name": fields.String,
+    "achievement_name": fields.String,
+    "professional_name": fields.String
+})
+
+post_organization_model = api.model("post organization model", {
+    "username": fields.String,
+    "companyname_name": fields.String,
+    "location_name": fields.String,
+    "field_name": fields.String,
+    "scale_name": fields.String,
+    "description_name": fields.String,
+})
+
+mood_check_model = api.model("check mood model", {
+    "InidividualId": fields.Integer
+})
+
+delete_offer_model = api.model("delete offer model", {
+    "OrganizationId": fields.Integer,
+    "OfferId": fields.Integer
+})
+
+preferoffer_model = api.model("get prefer offer model", {
+    "userId": fields.Integer
+})
+
+get_article_model = api.model("get article model", {
+    "Articel_ID": fields.Integer
+})
+
+#Jundi
 prefer_model = api.model("article model",{
     "articleID": fields.Integer
 })
@@ -66,3 +112,4 @@ delete_follow_org_model = api.model("org follow delete", {
 delete_follow_ind_model = api.model("ind follow delete", {
     "Individual": fields.List(fields.Integer)
 })
+
