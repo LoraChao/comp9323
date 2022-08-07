548bfff121d42cb1552d174647dd3d00a72c9c7d
MasterJzh <79051013+MasterJzh@users.noreply.github.com>
2022-08-07 13:43:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import Edit_details_personal from '@/pages/edit_profile_personal';
 import Edit_details_organizational from '@/pages/edit_profile_org';
 import JobEdit from '@/pages/job_modification';
 import Individual_Home from '@/pages/Individual_Home';
+import Organization_Home from '@/pages/Organization_Home';
 //import React from 'react';
 
 //import { Button } from 'antd';
@@ -52,6 +53,7 @@ function App() {
           <Route path='/editprofile_organizational' element={<Edit_details_organizational />}></Route>
           <Route path='/editjob' element={<JobEdit />}></Route>
           <Route path='/Individual_Home' element={<Individual_Home />}></Route>
+          <Route path='/Organization_Home' element={<Organization_Home />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
