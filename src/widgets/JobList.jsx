cccc9cb6a146f3c22e0917ae3147ed084c225fd8
MasterJzh <79051013+MasterJzh@users.noreply.github.com>
2022-08-07 15:30:01 +0800

    Add files via upload

--- src/widgets/JobList.jsx
+++ src/widgets/JobList.jsx
@@ -3,9 +3,19 @@ import Image1 from "../assets/pexels-photo-10282820.jpg";
 
 
 class ImageCard extends PureComponent{
+  getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+  }
 
   getOfferData() {
-  let url = "http://127.0.0.1:5000/offer/search/brief/1";
+    let url = "http://127.0.0.1:5000/auth/brief/organization/"+company_username;
   //window.alert(url)
   fetch(url, {
       method: "GET",
@@ -42,6 +52,9 @@ constructor(props) {
 
 }
 render(){
+  this.setState({
+    OrganizationId: this.getCookie('userid')
+    })
   if(this.state.flag === true){
   this.getOfferData()
   }
