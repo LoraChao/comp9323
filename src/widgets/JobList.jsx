acddd45dd5a56c090ad0ca4af9a576dc388329be
MasterJzh <79051013+MasterJzh@users.noreply.github.com>
2022-08-08 16:12:37 +0800

    Add files via upload

--- src/widgets/JobList.jsx
+++ src/widgets/JobList.jsx
@@ -2,30 +2,31 @@ import React, { PureComponent } from 'react';
 import Image1 from "../assets/pexels-photo-10282820.jpg";
 import { Button } from "antd";
 
-function AddNew() {
-
+function AddNew(props) {
+  const usertype = props.usertype
   function handleCheckArtileClick() {
     window.open(`/release`, { replace: true })
   }
+  if( usertype === 1){
 
   return (
-    <Button className="ml-8 bg-gray-900 px-4 py-2 rounded text-blue-50 flex items-center" onClick={() => { handleCheckArtileClick() }}>Add New</Button>
+    <Button className="ml-8 bg-gray-900 px-4 py-2 rounded text-blue-50 flex items-center" onClick={() => { handleCheckArtileClick() }}>Add New Offer</Button>
   )
 }
+return null}
 
 function JobCheckButton(props) {
-  // get article's id
-  const articleId = props.articleId
-
-  // jump with article's id
-  // const navigate = useNavigate()
+  const offerId = props.offerId
+  const usertype = offerId[1]
   function handleCheckArtileClick() {
-    window.open(`/editjob?offer_id=${articleId}`, { replace: true })
+    window.open(`/editjob?offer_id=${offerId[0]}`, { replace: true })
   }
+  if( usertype === 1){
+    return (
+      <Button  onClick={() => { handleCheckArtileClick() }}>Edit</Button>
+    )}
+  return null 
 
-  return (
-    <Button  onClick={() => { handleCheckArtileClick() }}>Edit</Button>
-  )
 }
 
 class ImageCard extends PureComponent {
@@ -42,8 +43,8 @@ class ImageCard extends PureComponent {
 
   getOfferData(company_username) {
     let url = "http://127.0.0.1:5000/offer/search/brief/" + company_username;
+    
     this.setState({ organizationid: company_username })
-    //window.alert(url)
     fetch(url, {
       method: "GET",
       headers: { "Content-Type": "application/json;charset=utf-8" },
@@ -93,8 +94,29 @@ class ImageCard extends PureComponent {
   }
   render() {
     var organizationid = this.getCookie('userid')
+    var usertype = this.getCookie("usertype")
+    var usertype_num = 0
+    var current_url = window.location.href;
+    let index = current_url.indexOf('=');
+    var read_company_id = current_url.substring(index + 1, current_url.length)
+    var usertype = this.getCookie("usertype")
+    if (organizationid === null) {
+      var organizationid = 0
+    }
+    if (usertype === "organization") {
+      var page_id = organizationid
+    } else {
+      var page_id = read_company_id
+    }
+  
+    if ( organizationid === null ) {
+      var organizationid = 1
+   }
+   if ( usertype === "organization" ) {
+    var usertype_num = 1
+ }
     if (this.state.flag === true) {
-      this.getOfferData(organizationid)
+      this.getOfferData(page_id)
       // const list = this.state.array
     }
     const array = Array.from(Array(this.state.len).keys())
@@ -102,7 +124,7 @@ class ImageCard extends PureComponent {
     return (
 
       <div>
-        <div><AddNew/></div>
+        <div><AddNew usertype = {usertype_num}/></div>
         <div className="mt-14 grid grid-cols-2 md:grid-cols-3 gap-6">
 
           {array.map((v) => (
@@ -113,11 +135,11 @@ class ImageCard extends PureComponent {
                 </div>
                 <div class="p-8">
 
-                  <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{this.state.company_data[v]["CompanyName"]}</a>
+                  <div class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{this.state.company_data[v]["CompanyName"]}</div>
                   <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{this.state.company_data[v]["Responsibility"]}</div>
                   <p class="mt-2 text-gray-500">{this.state.company_data[v]["Requirement"]}</p>
                   <div>
-                    <JobCheckButton articleId={this.state.company_data[v]["offerId"]} />
+                    <JobCheckButton offerId={[this.state.company_data[v]["offerId"],usertype_num]} />
                   </div>
                 </div>
               </div>
--- src/widgets/Organizations.jsx
+++ src/widgets/Organizations.jsx
@@ -1,92 +1,96 @@
-import React,{PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import SectionHeading from "./SectionHeading";
 import pic from "../assets/pexels-photo-10282820.jpg";
-import {Button} from "antd";
-function OrgButton(props){ 
-  
-  // jump with article's id
-  // const navigate = useNavigate()
-  function handleCheckArtileClick(){
-      window.open(`/Organization_Home`, {replace: true})
+import { Button } from "antd";
+function OrgButton(props) {
+
+  const organizationid = props.organizationid
+  // jump with params: 'job's id'
+  function handleCheckJobClick() {
+    window.open(`/Organization_Home?checkUserId=${organizationid}`, { replace: true })
   }
+
   return (
-    <Button className="ml-8 bg-gray-900 px-4 py-2 rounded text-blue-50 flex items-center" onClick={() => {handleCheckArtileClick()}}>More</Button>
- )
+    <Button onClick={() => { handleCheckJobClick() }}>More</Button>
+  )
 }
 
 
 
-class Organization extends PureComponent{
+class Organization extends PureComponent {
   getCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
     return null;
   }
   getOrganizationData(userid) {
-  let url = "http://127.0.0.1:5000/homepage/prefer_org/"+userid;
-  this.setState({userid:userid})
-  //window.alert(url)
-  fetch(url, {
+    let url = "http://127.0.0.1:5000/homepage/prefer_org/" + userid;
+    this.setState({ userid: userid })
+    //window.alert(url)
+    fetch(url, {
       method: "GET",
       headers: { "Content-Type": "application/json;charset=utf-8" },
-  }).then(res => res.json()).then(
+    }).then(res => res.json()).then(
       data => {
-          this.setState({organization_data: data["output"]})
-          this.setState({flag: false})
-          // window.alert(data['output'][0]['CompanyName'])
-          // return data
-          var return_value = data
-          //console.log(return_value)
-          return return_value
+        this.setState({ organization_data: data["output"] })
+        this.setState({ flag: false })
+        // window.alert(data['output'][0]['CompanyName'])
+        // return data
+        var return_value = data
+        console.log(return_value)
+        return return_value
       }
-  )
-}
-constructor(props) {
-  super(props)
-  this.state = {
-    flag: true,
-    userid:'',
-    organization_data:[{OrganizationName: "",Location: "",Field: "",Icon: ""},
-    {OrganizationName: "",Location: "",Field: "",Icon: ""},
-    {OrganizationName: "",Location: "",Field: "",Icon: ""},
-  ]}
-
-}
-render(){    
-  var UserId = this.getCookie('userid')
-  if ( UserId === null ) {
-    var UserId = 0
- }
-  if(this.state.flag === true){
-  this.getOrganizationData(UserId)
+    )
   }
-  return (
-    <div>
-      <SectionHeading
-        title="Maybe you are interested in these organizations"
-        subTitle=""
-      />
-      <div className="mt-14 grid grid-cols-2 md:grid-cols-3 gap-6">
-        {[0,1,2].map((v) => (
-          <div key={v} class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-            <div class="flex-shrink-0">
-              <img class="h-12 w-12" src={pic} alt="ChitChat Logo"></img>
+  constructor(props) {
+    super(props)
+    this.state = {
+      flag: true,
+      userid: '',
+      organization_data: [{ OrganizationId: "", OrganizationName: "", Location: "", Field: "", Icon: "" },
+      { OrganizationId: "", OrganizationName: "", Location: "", Field: "", Icon: "" },
+      { OrganizationId: "", OrganizationName: "", Location: "", Field: "", Icon: "" },
+      ]
+    }
+
+  }
+  render() {
+    var UserId = this.getCookie('userid')
+    if (UserId === null) {
+      var UserId = 0
+    }
+    if (this.state.flag === true) {
+      this.getOrganizationData(UserId)
+
+    }
+    return (
+      <div>
+        <SectionHeading
+          title="Maybe you are interested in these organizations"
+          subTitle=""
+        />
+        <div className="mt-14 grid grid-cols-2 md:grid-cols-3 gap-6">
+          {[0, 1, 2].map((v) => (
+            <div key={v} class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+              <div class="flex-shrink-0">
+                <img class="h-12 w-12" src={pic} alt="ChitChat Logo"></img>
+              </div>
+              <div>
+                <p class="text-xl font-medium text-black">{this.state.organization_data[v]['OrganizationName']}</p>
+                <p class="text-gray-500">{this.state.organization_data[v]['Location']}</p>
+                <OrgButton organizationid={this.state.organization_data[v]['OrganizationId']} />
+              </div>
             </div>
-            <div>
-              <p class="text-xl font-medium text-black">{this.state.organization_data[v]['OrganizationName']}</p>
-              <p class="text-gray-500">{this.state.organization_data[v]['Location']}</p>
-              <OrgButton />
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  );
-}}
+    );
+  }
+}
 
 export default Organization;
